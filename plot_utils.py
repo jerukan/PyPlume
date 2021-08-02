d77167bd3795f178378f9de086cd4bf2a7ba0584
Jerry Yan <jeyan@ucsd.edu>
2021-08-02 14:04:00 -0700

    Remove old plotting functions

--- plot_utils.py
+++ plot_utils.py
@@ -11,8 +11,6 @@ import numpy as np
 from parcels import plotting
 import xarray as xr
 
-import utils
-
 
 DEFAULT_PARTICLE_SIZE = 4
 
@@ -138,108 +136,6 @@ def draw_trajectories(datasets, names, domain=None, legend=True, scatter=True, s
     plt.title("Particle trajectories" if titlestr is None else titlestr)
 
     draw_plt(savefile=savefile, fig=fig, figsize=figsize)
-
-
-def scatter_particles_ax(ax, lats, lons, ages=None, agemax=None, part_size=DEFAULT_PARTICLE_SIZE):
-    """Draws particles from a list of coordinates on an axes"""
-    if ages is None:
-        ax.scatter(lons, lats, s=part_size)
-    else:
-        sc = ax.scatter(lons, lats, c=ages, edgecolor="k", vmin=0, vmax=agemax, s=part_size)
-        # TODO display alongside vector field bar
-        # plt.colorbar(sc)
-
-
-def draw_points_fieldset(lats, lons, show_time, hfrgrid, domain=None, line=False, savefile=None, part_size=DEFAULT_PARTICLE_SIZE):
-    """
-    Plot a bunch of points on top of a fieldset vector field. Option for plotting a line.
-    """
-    if domain is None:
-        domain = hfrgrid.get_domain()
-    _, fig, ax, _ = plotting.plotfield(field=hfrgrid.fieldset.UV, show_time=show_time,
-                                        domain=domain, land=True)
-    ax.scatter(lons, lats, s=part_size)
-    if line:
-        ax.plot(lons, lats)
-    draw_plt(savefile)
-
-
-def draw_particles_nc(nc, domain, time=None, label=None, show_time=None, land=True, savefile=None, vmax=None, field_vmax=None, part_size=DEFAULT_PARTICLE_SIZE):
-    """
-    Plots a bunch of particles to a map given the dataset is that single timestep of particles.
-    """
-    if "obs" in nc.dims:
-        raise Exception("netcdf file must have a single obs selected")
-    ext = [domain["W"], domain["E"], domain["S"], domain["N"]]
-    p_size = nc.dims["traj"]
-    lats = nc["lat"]
-    lons = nc["lon"]
-    ages = nc["lifetime"]
-
-    ages /= 86400  # seconds in a day
-
-    _, ax = get_carree_axis(domain, land)
-    gl = get_carree_gl(ax)
-
-    plt.scatter(lons, lats, s=part_size, label=label)
-
-    if time is None:
-        time = nc["time"][0].values
-    plt.title(f"Particle ages (days) {time}")
-
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05))
-
-    plt.draw()
-
-    if savefile is not None:
-        plt.savefig(savefile, bbox_inches="tight")
-        print(f"Plot saved to {savefile}", file=sys.stderr)
-        plt.close()
-
-
-def generate_simulation_plots(name, pf, hfrgrid=None, output_dir=utils.PICUTRE_DIR, domain=None, line_lats=None, line_lons=None, land=True, field_vmax=None, part_size=DEFAULT_PARTICLE_SIZE, figsize=None):
-    """
-    Generates a separate plot for each timestamp of the saved simulation.
-
-    Args:
-        name
-        pf (path-like or xr.Dataset): particle file output
-    """
-    if isinstance(pf, (str, Path)):
-        with xr.open_dataset(pf) as p_ds:
-            pf = p_ds
-    if domain is None:
-        if hfrgrid is None:
-            domain = {
-                "W": pf["lon"].min().values.item(),
-                "E": pf["lon"].max().values.item(),
-                "S": pf["lat"].min().values.item(),
-                "N": pf["lat"].max().values.item()
-            }
-            domain = pad_domain(domain, 0.0005)
-        else:
-            domain = hfrgrid.get_domain()
-    timestamps = pf["time"].isel(traj=0).values
-    plot_path = utils.create_path(Path(output_dir) / name)
-    num_pad = len(str(len(timestamps)))
-    for i, time in enumerate(timestamps):
-        savefile = str(plot_path / f"snap{str(i).zfill(num_pad)}.png")
-        if hfrgrid is None:
-            fig, ax = get_carree_axis(domain, land=land)
-            get_carree_gl(ax)
-        else:
-            show_time = int((time - hfrgrid.times[0]) / np.timedelta64(1, "s"))
-            if show_time < 0:
-                raise ValueError("Particle simulation time domain goes out of bounds")
-            _, fig, ax, _ = plotting.plotfield(field=hfrgrid.fieldset.UV, show_time=show_time,
-                                            domain=domain, land=land, vmin=0, vmax=field_vmax,
-                                            titlestr="Particles and ")
-        ax.scatter(pf["lon"].isel(obs=i), pf["lat"].isel(obs=i), s=part_size)
-        if line_lats is not None and line_lons is not None:
-            for j in range(len(line_lats)):
-                ax.plot(line_lons[j], line_lats[j])
-        draw_plt(savefile=savefile, fig=fig, figsize=figsize)
-    return plot_path
 
 
 def plot_particles(

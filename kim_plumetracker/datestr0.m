04c9c88f24da6363cdb9ecc8f1b65c167d5293a8
Jerry <jeyan@ucsd.edu>
2023-01-13 14:31:31 -0800

    Remove matlab scripts and other unused files/notebooks

--- kim_plumetracker/datestr0.m
+++
@@ -1,9 +0,0 @@
-function [YYYY, MM, DD, hh, mm] = datestr0(t)
-d = datevec(t); nt = length(t); 
-if nt ~= 1, error ('the length of time should be one !'); end
-
-YYYY = num2str(d(1));
-if d(2)< 10,  MM = ['0',num2str(d(2))]; ,else, MM = num2str(d(2)); end
-if d(3)< 10,  DD = ['0',num2str(d(3))]; ,else, DD = num2str(d(3)); end
-if d(4)< 10,  hh = ['0',num2str(d(4))]; ,else, hh = num2str(d(4)); end
-if d(5)< 10,  mm = ['0',num2str(d(5))]; ,else, mm = num2str(d(5)); end
